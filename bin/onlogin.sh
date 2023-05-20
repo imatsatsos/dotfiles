6c2bc78e335ad45c191a6cc6da8be7e4118e1e08
imatsatsos <ipmatsatsos@gmail.com>
2023-05-20 08:57:27 +0300

    added new function to apply refresh rate

--- bin/onlogin.sh
+++ bin/onlogin.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Here live some on-login things I want to be run
-# These are: setting touchpad scroll speed, merging .Xresources
+# These are: 
+# setting touchpad scroll speed, 
+# merging .Xresources,
+# setting BENQ monitor refresh to 100 Hz
 
 set_touchpad_scrollspeed( ){
 	# find touchpad device from xinput
@@ -17,7 +20,15 @@ set_touchpad_scrollspeed( ){
 	xinput set-prop $id "libinput Scrolling Pixel Distance" 50
 }
 
+set_benq_refresh() {
+    # find port benq is connected
+    port=$( xrandr -q | grep 'HDMI' | grep -Ev 'disconnected' | awk '{print $1}' )
+    # set refresh rate
+    xrandr --output $port --mode 1920x1080 --rate 100
+}
+
 sleep 1
 set_touchpad_scrollspeed
 xrdb -merge ~/.Xresources
+set_benq_refresh
 notify-send "onlogin.sh run successfully!"
