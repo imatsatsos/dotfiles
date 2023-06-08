f1220a80896c3a460dbd5b4f558e3a4aefd2f05a
imatsatsos <ipmatsatsos@gmail.com>
2023-06-08 13:36:41 +0300

    new

---
+++ bin/change_sink.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# This script will cycle through available output sinks
+
+set -e
+
+default_sink=$(pactl info | grep "Default Sink:" | cut '-d ' -f3)
+sinks=$(pactl list short sinks | cut -f2)
+
+# for wrap-around
+sinks="$sinks
+$sinks"
+
+next_sink=$(echo "$sinks" | awk "/$default_sink/{getline x;print x;exit;}")
+
+pactl set-default-sink "$next_sink"
+pactl list short sink-inputs | \
+  cut -f1 | \
+  xargs -I{} pactl move-sink-input {} "$next_sink"
